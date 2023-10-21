809f9c91de1c1355d62abf61dfce8a9e50b8389e
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2023-10-21 14:26:29 +0300

    Task 1 wad added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const basicLightboxInstance = basicLightbox.create(
+  `<img class="js-image" width="800" height="600">`,
+  {
+    onShow: () => document.addEventListener('keydown', handleEscClick),
+    onClose: () => document.removeEventListener('keydown', handleEscClick)
+  }
+);
+
+gallery.addEventListener('click', handleImageClick);
+
+function createItems(galleryItems) {
+  const items = galleryItems
+    .map(({ preview, description, original }) => (
+      `<li class="gallery__item">
+          <a class="gallery__link" href="large-image.jpg">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    ))
+    .join('');
+
+  gallery.insertAdjacentHTML('afterbegin', items);
+}
+
+function handleImageClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) return;
+
+  basicLightboxInstance.show(
+    () => document
+      .querySelector('.js-image')
+      .setAttribute('src', `${event.target.dataset.source}`)
+  );
+}
+
+function handleEscClick(event) {
+  if (event.key !== 'Escape') return;
+
+  basicLightboxInstance.close();
+}
+
+createItems(galleryItems);
